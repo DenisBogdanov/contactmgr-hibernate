f26638f28afa9b3f866cf9c550215e8adc80a291
Denis Bogdanov <bogdanov.math@gmail.com>
2018-01-22 22:23:00 +0300

    Build Hibernate SessionFactory.

--- src/main/java/ru/bogdanium/contactmgr/App.java
+++ src/main/java/ru/bogdanium/contactmgr/App.java
@@ -1,4 +1,21 @@
 package ru.bogdanium.contactmgr;
 
+import org.hibernate.SessionFactory;
+import org.hibernate.boot.MetadataSources;
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
+import org.hibernate.service.ServiceRegistry;
+
+
 public class App {
+    private static final SessionFactory sessionFactory = buildSessionFactory();
+
+    private static SessionFactory buildSessionFactory() {
+        final ServiceRegistry registry =
+                new StandardServiceRegistryBuilder().configure().build();
+        return new MetadataSources(registry).buildMetadata().buildSessionFactory();
+    }
+
+    public static void main(String[] args) {
+
+    }
 }
