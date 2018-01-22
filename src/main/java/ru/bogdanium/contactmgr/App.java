d707bec9187e4ab968e1805eaefcdadfde189169
Denis Bogdanov <bogdanov.math@gmail.com>
2018-01-22 22:29:38 +0300

    Use builder pattern for creating Contact objects.

--- src/main/java/ru/bogdanium/contactmgr/App.java
+++ src/main/java/ru/bogdanium/contactmgr/App.java
@@ -4,10 +4,11 @@ import org.hibernate.SessionFactory;
 import org.hibernate.boot.MetadataSources;
 import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
 import org.hibernate.service.ServiceRegistry;
+import ru.bogdanium.contactmgr.model.Contact;
 
 
 public class App {
-    private static final SessionFactory sessionFactory = buildSessionFactory();
+//    private static final SessionFactory sessionFactory = buildSessionFactory();
 
     private static SessionFactory buildSessionFactory() {
         final ServiceRegistry registry =
@@ -16,6 +17,10 @@ public class App {
     }
 
     public static void main(String[] args) {
-
+        Contact contact = new Contact.ContactBuilder("Denis", "Bogdanov")
+                .withEmail("denis@bogdanium.ru")
+                .withPhone(12345678L)
+                .build();
+        System.out.println(contact);
     }
 }
